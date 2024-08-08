066c1dc40d8e592c0cd962c5db27a611aa4dce39
Jay Thanki <thankijay99@gmail.com>
2024-08-08 21:07:54 +0100

    Built the tree node and a file system struct

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"vfs-shell/internal/vfs"
 )
 
 func main() {
 	fmt.Println("Hello World!!")
+	fs := vfs.NewFileSystem()
+	log.Println(*(fs.Root))
 }
--- internal/vfs/vfs.go
+++ internal/vfs/vfs.go
@@ -1,9 +1,30 @@
 package vfs
 
 import (
-	"log"
+	"time"
 )
 
-func Main() {
-	log.Println("Building vfs")
+// node of a tree
+type File struct {
+	Name    string
+	Content []byte
+	IsDir   bool
+	Mode    string
+	ModTime time.Time
+	Files   []*File
+}
+
+type FileSystem struct {
+	Root *File
+}
+
+func NewFileSystem() *FileSystem {
+	root := &File{
+		Name:    "/",
+		IsDir:   true,
+		Mode:    "drwxr-xr-x",
+		ModTime: time.Now(),
+	}
+
+	return &FileSystem{Root: root}
 }

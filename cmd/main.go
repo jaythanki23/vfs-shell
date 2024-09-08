ef02e4d397296d8cdeeb0c813fc31f5bec4c3f3d
Jay Thanki <thankijay99@gmail.com>
2024-09-08 23:00:47 +0100

    Installed mingw64 and wrote basic code to initialize a terminal.

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,13 @@
 package main
 
 import (
-	"vfs-shell/internal/shell"
-	"vfs-shell/internal/vfs"
+	"vfs-shell/internal/terminal"
 )
 
 func main() {
-	fs := vfs.NewFileSystem()
+	// fs := vfs.NewFileSystem()
 
-	shell.StartShell(fs)
+	// shell.StartShell(fs)
 
-	// if err := fs.CreateFile("Hello", []byte("Hi Jay")); err != nil {
-	// 	log.Printf("Error creating file: %v", err.Error())
-	// }
-
-	// // log.Println(fs.CurrentDir.Files[0])
-
-	// if err := fs.CreateDir("project"); err != nil {
-	// 	log.Printf("Error creating directory: %v", err.Error())
-	// }
-
-	// // log.Println(fs.CurrentDir.Files[1])
-
-	// if err := fs.ChangeDir("/project"); err != nil {
-	// 	log.Printf("Error changing directory: %v", err.Error())
-	// }
-
-	// log.Println("Current dir: ", fs.CurrentDir)
+	terminal.StartTerminal()
 }
